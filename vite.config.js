4bf9e0cc225dea20c649f561d302c41a330aeca1
Oladokun <65833243+oladokun-o@users.noreply.github.com>
2025-06-29 16:12:36 +0100

    chore: add Vercel adapter and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	kit: {
+		adapter: adapter()
+	}
 });
